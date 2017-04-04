5ed2791376c10b6158e1053e17acdf1c6df6b4aa
zhangyong <zhangyong@zplay.com>
2017-04-04 23:38:34 +0800

    add test

--- statistics_test.go
+++ statistics_test.go
@@ -1,32 +1,40 @@
 package goutil
 
 import (
-	"strconv"
+	"encoding/json"
 	"testing"
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func MockGetTime() time.Time {
-	return time.Now()
+	t1, _ := time.Parse("2006-01-02 15:04:05", "2017-08-29 12:12:00")
+	return t1
 }
 
 func TestStatistics(t *testing.T) {
-	statis := NewStatistics(10000, 10, "2006-01-02 15:04:05", getTime)
-	var i int
-	i = 0
-	for i < 100000 {
-		item := Item{
-			Type:    "api",
-			SubType: "get",
-			Key:     "user" + strconv.Itoa(i%19),
-		}
-		i += 1
-		statis.AddItem(item)
-	}
 
-	time.Sleep(time.Second)
-	info := statis.GetInfoByTypeAndSubType("api", "get")
-	spew.Dump(info)
+	Convey("Test statistics", t, func() {
+		statis := NewStatistics(10000, 10, "2006-01-02 15:04:05", MockGetTime)
+		var i int
+		i = 0
+		for i < 1000 {
+			item := Item{
+				Type:    "api",
+				SubType: "get",
+				Key:     "user",
+			}
+			i += 1
+			statis.AddItem(item)
+		}
+
+		time.Sleep(time.Second)
+		expectStr := `[{"time":"2017-08-29 12:12:00","user":1000}]`
+
+		info := statis.GetInfoByTypeAndSubType("api", "get")
+		actualJson, _ := json.Marshal(info)
+
+		So(string(actualJson), ShouldEqual, expectStr)
+	})
 }
