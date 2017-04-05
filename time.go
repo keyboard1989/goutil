7abf9a2fadbac792be308fb0848a12aa72b6fbdf
zhangyong <zhangyong@zplay.com>
2017-04-05 14:43:34 +0800

    add get time range function

--- time.go
+++ time.go
@@ -1,7 +1,44 @@
 package goutil
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
+// GetCurrentTimeStr is used to get current time str
+// with format "2006-01-02 15:04:05"
 func GetCurrentTimeStr() string {
 	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+// GetDayRange is used to get a range of start and end time
+// start string eg:2017-01-02
+// end   string eg:2017-01-22
+// format string eg: 2006-01-02
+// result: ["2017-01-02","2017-01-03",...,"2017-01-21"]
+func GetDayRange(start string, end string, format string) ([]string, error) {
+	ret := []string{}
+	var err error
+
+	startTime, err := time.Parse(format, start)
+	if err != nil {
+		return ret, err
+	}
+
+	endTime, err := time.Parse(format, end)
+	if err != nil {
+		return ret, err
+	}
+
+	if startTime.Unix() > endTime.Unix() {
+		return ret, errors.New("start bigger than end")
+	}
+
+	for !startTime.Equal(endTime) {
+		timeStr := startTime.Format(format)
+		ret = append(ret, timeStr)
+		startTime = startTime.Add(24 * time.Hour)
+	}
+
+	return ret, nil
 }
--- time_test.go
+++ time_test.go
@@ -3,6 +3,8 @@ package goutil
 import (
 	"testing"
 	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestGetCurrentTimeStr(t *testing.T) {
@@ -12,4 +14,69 @@ func TestGetCurrentTimeStr(t *testing.T) {
 	if expectStr != actualStr {
 		t.Errorf("GetCurrentTimeStr() func error,actual:%s expect:%s", actualStr, expectStr)
 	}
+}
+
+func TestGetDayRange(t *testing.T) {
+	Convey("Test Get day Range", t, func() {
+
+		Convey("Test right situation", func() {
+			start := "2017-01-02"
+			end := "2017-01-05"
+			format := "2006-01-02"
+			expectResult := []string{"2017-01-02", "2017-01-03", "2017-01-04"}
+
+			actualResult, err := GetDayRange(start, end, format)
+
+			So(err, ShouldBeNil)
+			So(actualResult, ShouldResemble, expectResult)
+		})
+
+		Convey("Test format error", func() {
+			start := "2017-01-02"
+			end := "2017-01-05"
+			format := "200601-02"
+			expectResult := []string{}
+
+			actualResult, err := GetDayRange(start, end, format)
+
+			So(err, ShouldNotBeNil)
+			So(actualResult, ShouldResemble, expectResult)
+		})
+
+		Convey("Test start format error", func() {
+			start := "201701-02"
+			end := "2017-01-05"
+			format := "2006-01-02"
+			expectResult := []string{}
+
+			actualResult, err := GetDayRange(start, end, format)
+
+			So(err, ShouldNotBeNil)
+			So(actualResult, ShouldResemble, expectResult)
+		})
+
+		Convey("Test end format error", func() {
+			start := "2017-01-02"
+			end := "201701-05"
+			format := "2006-01-02"
+			expectResult := []string{}
+
+			actualResult, err := GetDayRange(start, end, format)
+
+			So(err, ShouldNotBeNil)
+			So(actualResult, ShouldResemble, expectResult)
+		})
+
+		Convey("Test start bigger than end", func() {
+			start := "2017-02-02"
+			end := "2017-01-05"
+			format := "2006-01-02"
+			expectResult := []string{}
+
+			actualResult, err := GetDayRange(start, end, format)
+
+			So(err, ShouldNotBeNil)
+			So(actualResult, ShouldResemble, expectResult)
+		})
+	})
 }
