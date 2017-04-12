1aaa0457fb3100c99196aa66af370542e8b2ebcd
zhangyong <zhangyong@zplay.com>
2017-04-12 13:58:54 +0800

    add random method

---
+++ random.go
@@ -0,0 +1,21 @@
+package goutil
+
+import (
+	"math/rand"
+	"time"
+)
+
+func GetRandInt32() int32 {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Int31()
+}
+
+func GetRandInt() int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Int()
+}
+
+func GetResidueRandy(num int) int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Int() % num
+}
